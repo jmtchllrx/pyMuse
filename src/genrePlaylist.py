fb1b435eb6e3ff8a7d3ac3d00821e04c1719ff60
apprentice <apprentice@apprentice.(none)>
2015-02-17 13:52:37 -0500

    making playlist a class

--- src/genrePlaylist.py
+++ src/genrePlaylist.py
@@ -6,40 +6,45 @@ from os.path import join
 
 from hsaudiotag import auto
 
-FORMATS = ('.mp3', '.wma', 'flac', '.ogg', '.aif', 'aiff')
-
-#TODO
-# 1. Add filter functions to remove songs at user request
-
-def getByGenre(genre, directory):
-    """Searches cwd and subdirectories for music files with matching
-    genre ID3 tags and returns a list of them
-    """
+class Playlist():
+    FORMATS = ('.mp3', '.wma', 'flac', '.ogg', '.aif', 'aiff')
     playlist = []
 
-    #TODO 
-    # 1. turn this into cleaner list comprehension
-    # 2. change to absolute rather than relative paths
-    #    in case user wants to output playlist
-    #    in another directory
-    for root, dirs, files in os.walk(directory):
-        for item in files:
-            if item[len(item)-4:len(item)] in FORMATS:
-                if auto.File(join(root, item)).genre == genre:
-                    playlist.append(join(root, item))
+    #TODO
+    # 1. Add filter functions to remove songs at user request
 
-    return playlist
+    def getByGenre(genre, directory):
+        """Searches cwd and subdirectories for music files with matching
+        genre ID3 tags and returns a list of them
+        """
+
+        #TODO 
+        # 1. turn this into cleaner list comprehension
+        # 2. change to absolute rather than relative paths
+        #    in case user wants to output playlist
+        #    in another directory
+        for root, dirs, files in os.walk(directory):
+            for item in files:
+                if item[len(item)-4:len(item)] in FORMATS:
+                    if auto.File(join(root, item)).genre == genre:
+                        playlist.append(join(root, item))
+
+        return playlist
 
 
-def generatePlaylist(playlist, directory, name="playlist", randomize=True):
-    """Generates m3u file to parameter specification, no return value"""
-    if randomize:
-        random.shuffle(playlist)
+    def filterArtist(artist):
+        playlist = filter(lambda track: auto.File(track).artist != artist, playlist)
 
-    playlistFile = open(join(directory, name + ".m3u"), "w")
-    playlistFile.write("#EXTM3U\n")
-    for song in playlist:
-        playlistFile.write(song + "\n")
 
-    playlistFile.flush()
-    playlistFile.close()
+    def generatePlaylist(directory, name="playlist", randomize=True):
+        """Generates m3u file to parameter specification, no return value"""
+        if randomize:
+            random.shuffle(playlist)
+
+        playlistFile = open(join(directory, name + ".m3u"), "w")
+        playlistFile.write("#EXTM3U\n")
+        for song in playlist:
+            playlistFile.write(song + "\n")
+
+        playlistFile.flush()
+        playlistFile.close()
--- src/pyMuseConsole.py
+++ src/pyMuseConsole.py
@@ -6,18 +6,20 @@ import genrePlaylist
 genre = input("Genre?\n")
 musicDir = input("Different directory? (Hit return for cwd)\n")
 
+playlist = Playlist
+
 #TODO
 # 1. Just makes this better
 
 print("working...")
 if len(musicDir) > 0:
-    playlist = genrePlaylist.getByGenre(genre, musicDir)
+    playlist.getByGenre(genre, musicDir)
 else:
-    playlist = genrePlaylist.getByGenre(genre, os.getcwd())
+    playlist.getByGenre(genre, os.getcwd())
 
 if input("Edit output directory or name? (y/n)") == "y":
     outputDir = input("Enter directory: ")
     name = input("Enter playlist name: ")
-    genrePlaylist.generatePlaylist(playlist, outputDir, name)
+    playlist.generatePlaylist(playlist, outputDir, name)
 else:
-    genrePlaylist.generatePlaylist(playlist, os.getcwd())
+    playlist.generatePlaylist(playlist, os.getcwd())
