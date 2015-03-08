f0f8d1875b430cfcaa11eca016a3fd9515cef27d
jmtchllrx <jmitchellrx@gmail.com>
2015-03-08 04:12:55 -0400

    Should now work for all filename characters

--- README.md
+++ README.md
@@ -3,13 +3,21 @@ Simple python library/app for managing music directory
 
 All this program will do currently is give simple prompts and output a playlist of the genre you input.
 
+
 Use:
+
 Run src/pyMuseConsole.py
 
+
+
+
 TODO
+
 Capability to change directory structure based on arbitrary genre/artist/album/title tags
 
 Think of revolutionary way to organize music the way I want
+
+
 
 
 Using the hsaudiotag library
--- src/genrePlaylist.py
+++ src/genrePlaylist.py
@@ -10,23 +10,28 @@ from hsaudiotag import auto
 class Playlist():
 
 
-    FORMATS = ('.mp3', '.wma', 'flac', '.ogg', '.aif', 'aiff')
+    FORMATS = ('.mp3', '.wma', 'flac', '.ogg', '.aif', 'aiff', '.m4a')
     playlist = []
 
 
     def getByGenre(self, genre, directory):
         """Searches cwd and subdirectories for music files with matching
-        genre ID3 tags and returns a list of them
+        genre ID3 tags
         """
 
-        # TODO 
-        # turn into list comprehension
-
-        for root, dirs, files in os.walk(directory):
+        for root, dirs, files in os.walk(directory.encode('utf-8')):
             for item in files:
-                if item[len(item)-4:len(item)] in self.FORMATS:
-                    if auto.File(join(root, item)).genre == genre:
-                        self.playlist.append(join(root, item))
+                if item[len(item)-4:len(item)].decode('utf-8') in self.FORMATS:
+
+                    # Basically, the hsaudiotag library does not handle text
+                    # correctly. This bytes-file opening weirdness is the only way I can get
+                    # it to handle non-ascii characters
+                    # (The only good library is GPL, so I'm sticking with hsaudiotag)
+                    path = root + b'/' + item
+                    currentFile = open(path, 'rb')
+                    if auto.File(currentFile).genre == genre:
+                        self.playlist.append(path)
+                    currentFile.close()
 
 
     def filterArtist(self, artist):
@@ -40,10 +45,11 @@ class Playlist():
         if randomize:
             random.shuffle(self.playlist)
 
-        playlistFile = open(join(directory, name + ".m3u"), "w")
-        playlistFile.write("#EXTM3U\n")
+        # See comment in getByGenre. I'm not even entirely sure why all this bytes stuff works
+        playlistFile = open(join(directory, name + ".m3u"), "wb")
+        playlistFile.write(b"#EXTM3U\n")
         for song in self.playlist:
-            playlistFile.write(song + "\n")
+            playlistFile.write(song + b"\n")
 
         playlistFile.flush()
         playlistFile.close()
